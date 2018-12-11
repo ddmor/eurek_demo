2913fe4dc1b7213af1f7ddc727fad893cacbd771
yangxiang <yxiang@ctrip.com>
2018-12-11 15:59:53 +0800

    add test api

---
+++ ch2-1-eureka-client/src/main/java/cn/springcloud/book/controller/TestController.java
@@ -0,0 +1,15 @@
+package cn.springcloud.book.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    // 服务消费者对位提供的服务
+    @GetMapping("/test")
+    public String findByIdByEurekaServer() {
+        return "ok";
+
+    }
+}
